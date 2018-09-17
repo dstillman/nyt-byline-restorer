f34d00f5c2283315c1ef8970be575b560ea45fc6
Dan Stillman <dan@danstillman.com>
2018-09-17 16:42:50 -0400

    Show bylines when clicking to homepage from another section

--- bylines.js
+++ bylines.js
@@ -59,57 +59,59 @@ function addBylines(urlMap) {
 	//console.log(`Present: ${present}  Added: ${added}  Not Found: ${notFound}`);
 }
 
-fetch(feedURL)
-	.then(response => response.text())
-	.then(text => {
-		var urlMap = new Map();
-		
-		let doc = (new DOMParser).parseFromString(text, 'text/xml');
-		let items = doc.querySelectorAll('item');
-		for (let item of items) {
-			// Get relative paths without query strings from the feed item URLs
-			let url = item.querySelector('link:not([rel])').textContent;
-			url = url.match(/https:\/\/[^\/]+([^\?]+)/)[1];
+if (window.location.href.split('?')[0] == 'https://www.nytimes.com/') {
+	fetch(feedURL)
+		.then(response => response.text())
+		.then(text => {
+			var urlMap = new Map();
 			
-			// Ignore URLs we already have
-			if (urlMap.has(url)) {
-				continue;
+			let doc = (new DOMParser).parseFromString(text, 'text/xml');
+			let items = doc.querySelectorAll('item');
+			for (let item of items) {
+				// Get relative paths without query strings from the feed item URLs
+				let url = item.querySelector('link:not([rel])').textContent;
+				url = url.match(/https:\/\/[^\/]+([^\?]+)/)[1];
+				
+				// Ignore URLs we already have
+				if (urlMap.has(url)) {
+					continue;
+				}
+				
+				// Opinion pieces already show authors
+				if (url.includes('/opinion/')) {
+					continue;
+				}
+				
+				// Fix capitalization of author names
+				let authorString = item.querySelector('creator').textContent
+					.split(/ and /g)
+					.map((author) => {
+						return author
+							.toLowerCase()
+							.split(' ')
+							.map(name => name.charAt(0).toUpperCase() + name.substr(1))
+							.join(' ');
+					})
+					.join(' and ');
+				if (!authorString) {
+					continue;
+				}
+				
+				urlMap.set(
+					url,
+					{
+						id: null,
+						authorString
+					});
 			}
 			
-			// Opinion pieces already show authors
-			if (url.includes('/opinion/')) {
-				continue;
-			}
-			
-			// Fix capitalization of author names
-			let authorString = item.querySelector('creator').textContent
-				.split(/ and /g)
-				.map((author) => {
-					return author
-						.toLowerCase()
-						.split(' ')
-						.map(name => name.charAt(0).toUpperCase() + name.substr(1))
-						.join(' ');
-				})
-				.join(' and ');
-			if (!authorString) {
-				continue;
-			}
-			
-			urlMap.set(
-				url,
-				{
-					id: null,
-					authorString
-				});
-		}
-		
-		addBylines(urlMap);
-		// Check all bylines again after a short delay, both for elements that were added after
-		// the page load and to restore bylines that were removed by JS updating components on
-		// the page (particularly when clicking Back from an article).
-		setTimeout(() => addBylines(urlMap), 750);
-		setTimeout(() => addBylines(urlMap), 2500);
-		setTimeout(() => addBylines(urlMap), 5000);
-	})
-	.catch(e => console.log(e));
+			addBylines(urlMap);
+			// Check all bylines again after a short delay, both for elements that were added after
+			// the page load and to restore bylines that were removed by JS updating components on
+			// the page (particularly when clicking Back from an article).
+			setTimeout(() => addBylines(urlMap), 750);
+			setTimeout(() => addBylines(urlMap), 2500);
+			setTimeout(() => addBylines(urlMap), 5000);
+		})
+		.catch(e => console.log(e));
+}
